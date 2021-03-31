f024d7bf1f4d2cc224e070a6da9192e791381ef4
Roberto Fortes <robertbernd23@gmail.com>
2021-03-31 10:00:55 -0300

    UseEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,61 +1,58 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-import SectionTitle from "../../components/layout/SectionTitle";
+import SectionTitle from '../../components/layout/SectionTitle'
 
-
-function calcFactorial (n) {
-    if(n < 0) return -1
-    if(n === 0) return 1
-    return calcFactorial(n-1) * n
+function calcFatorial(num) {
+    const n = parseInt(num)
+    if (n < 0) return -1
+    if (n === 0) return 1
+    return calcFatorial(n - 1) * n
 }
 
 const UseEffect = (props) => {
-
     const [number, setNumber] = useState(1)
-    const [factorial, setFactorial] = useState(1)
-    const [numberDesafio, setNumberDesafio] = useState(0)
-    const [isPar, setIsPar] = useState(true)
+    const [fatorial, setFatorial] = useState(1)
 
-    useEffect(function (){
-        setFactorial(calcFactorial(number))
+    useEffect(function () {
+        setFatorial(calcFatorial(number))
     }, [number])
 
-    useEffect(function (){
-        if(factorial > 1000000){
-            document.title = 'Eita!!!'
+    useEffect(function () {
+        if (fatorial > 100000) {
+            document.title = "Eita!"
         }
-    }, [factorial])
+    }, [fatorial])
+    //Ex #02
 
-    useEffect(function (){
-        if(numberDesafio % 2 === 0){
-            setIsPar(true)
-        } else{
-            setIsPar(false)
-        }
-    }, [numberDesafio])
+    const [status, setStatus] = useState("Ímpar")
+
+    useEffect(function() {
+        setStatus(number % 2 === 0 ? "Par" : "Ímpar")
+    }, [number] )
 
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-            <SectionTitle title='Exercício #01'/>
-            <div className='center'>
+                subtitle="Permite executar efeitos colaterais em componentes funcionais!" />
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
                 <div>
-                    <span className='text'>Fatorial: </span>
-                    <span className='text red'>{factorial === -1 ? 'Não existe fatorial de número negativo!' : factorial}</span>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{fatorial === -1 ? 'Não existe' : fatorial}</span>
                 </div>
-                <input type='number' className='input' value={number} onChange={e => setNumber(e.target.value)}/>
+                <input type="number" className="input"
+                    value={number}
+                    onChange={e => setNumber(e.target.value)} />
             </div>
 
-            <SectionTitle title='Exercício #02'/>
-            <div className='center'>
-                <span className='text'>É o que?  </span>
-                <span className='text red'>{isPar === true ? 'É par!' : 'É impar!'}</span>
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{status}</span>
+                </div>
             </div>
-            <input type='number' className='input' value={numberDesafio} onChange={e => setNumberDesafio(e.target.value)}/>
-
         </div>
     )
 }
